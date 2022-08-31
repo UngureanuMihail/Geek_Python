a570c3212e8deb0c07fe0d6ad13ecef22305a43d
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-08-31 15:35:40 +0300

    Добавление определения победителя

--- Lesson_5/Tic_Tac_Toe.py
+++ Lesson_5/Tic_Tac_Toe.py
@@ -18,6 +18,63 @@ def print_game_board():
             print("", gameBoard[x][y], end=" |")
     print("\n+---+---+---+")
 
+   
+# Функция для определения победителя
+def check_for_winner(gameBoard):
+    # По оси Х
+    if gameBoard[0][0] == 'X' and gameBoard[0][1] == 'X' and gameBoard[0][2] == 'X':
+        print("Пользователь победил!")
+        return "X"
+    elif gameBoard[0][0] == 'O' and gameBoard[0][1] == 'O' and gameBoard[0][2] == 'O':
+        print("Компьютер победил!")
+        return "O"
+    elif gameBoard[1][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[1][2] == 'X':
+        print("Пользователь победил!")
+        return "X"
+    elif gameBoard[1][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[1][2] == 'O':
+        print("Компьютер победил!")
+        return "O"
+    elif gameBoard[2][0] == 'X' and gameBoard[2][1] == 'X' and gameBoard[2][2] == 'X':
+        print("Компьютер победил!")
+        return "X"
+    elif gameBoard[2][0] == 'O' and gameBoard[2][1] == 'O' and gameBoard[2][2] == 'O':
+        print("Компьютер победил!")
+        return "O"
+    # По оси У
+    if gameBoard[0][0] == 'X' and gameBoard[1][0] == 'X' and gameBoard[2][0] == 'X':
+        print("Пользователь победил!")
+        return "X"
+    elif gameBoard[0][0] == 'O' and gameBoard[1][0] == 'O' and gameBoard[2][0] == 'O':
+        print("Компьютер победил!")
+        return "O"
+    elif gameBoard[0][1] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][1] == 'X':
+        print("Пользователь победил!")
+        return "X"
+    elif gameBoard[0][1] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][1] == 'O':
+        print("Компьютер победил!")
+        return "O"
+    elif gameBoard[0][2] == 'X' and gameBoard[1][2] == 'X' and gameBoard[2][2] == 'X':
+        print("Пользователь победил!")
+        return "X"
+    elif gameBoard[0][2] == 'O' and gameBoard[1][2] == 'O' and gameBoard[2][2] == 'O':
+        print("Компьютер победил!")
+        return "O"
+    # По диагонали
+    elif gameBoard[0][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][2] == 'X':
+        print("Пользователь победил!")
+        return "X"
+    elif gameBoard[0][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][2] == 'O':
+        print("Компьютер победил!")
+        return "O"
+    elif gameBoard[0][2] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][0] == 'X':
+        print("Пользователь победил!")
+        return "X"
+    elif gameBoard[0][2] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][0] == 'O':
+        print("Компьютер победил!")
+        return "O"
+    else:
+        return "N"
+
 
 # Фунция для хода (изменение доски)
 def modify_array(num, turn):
@@ -49,21 +106,26 @@ while not finish:
     # Ход пользователя
     if turnCounter % 2 == 0:  # В случае остатка деления значения хода на 2 == 0, значит ход пользовтеля
         print_game_board()
-        numberPicked = int(input("\nChoose a number [1-9]: "))  # Ввод числа
+        numberPicked = int(input("\nВведите цифру [1-9]: "))  # Ввод числа
         if numberPicked >= 1 or numberPicked <= 9:  # Условине для ввода числа из диапазона
             modify_array(numberPicked, 'X')  # Изменение доски
             possibleNumbers.remove(numberPicked)  # Удаление номера из списка возможных
         else:
-            print("Invalid input. Please try again.")  # В другом случае сообщение о ошибке
+            print("Неверное число. Пожалуйста попробуйте еще раз.")  # В другом случае сообщение о ошибке
         turnCounter += 1
 
     # Ход компьютера
     else:
         while True:
             cpu_choice = random.choice(possibleNumbers)  # Случайный выбор комьютера
-            print("\nCpu choice: ", cpu_choice)
+            print("\nВыбор компьютера: ", cpu_choice)
             if cpu_choice in possibleNumbers:
                 modify_array(cpu_choice, 'O')
                 possibleNumbers.remove(cpu_choice)  # Удаление номера из списка возможных
                 turnCounter += 1
                 break
+
+    winner = check_for_winner(gameBoard)
+    if winner != "N":
+        print("\nКонец. Спасибо за игру :)")
+        break
