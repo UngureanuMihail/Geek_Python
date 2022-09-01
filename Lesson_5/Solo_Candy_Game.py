28cdbfae1d67e2d248e9eeadb418e7b8cf5c3534
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-09-01 20:50:45 +0300

    Изменение метода выбора числа компьютером

--- Lesson_5/Solo_Candy_Game.py
+++ Lesson_5/Solo_Candy_Game.py
@@ -2,6 +2,7 @@
 from random import randint as randint
 
 numsOfCandies = 2021
+max_take = 28
 counter = 0
 
 
@@ -9,27 +10,25 @@ def player_choice(numsOfCandies):
     if numsOfCandies == 0:
         return -1
     takenCandies = int(input("Enter how many candies you are taking away: "))
-    while takenCandies <= 0 or takenCandies > 28:
-        takenCandies = int(input('You can take between 1 and 28 candies, please try again :'))
+    while takenCandies <= 0 or takenCandies > max_take:
+        takenCandies = int(input(f'You can take between 1 and {max_take} candies, please try again :'))
         if numsOfCandies < takenCandies:
             print(f' {numsOfCandies} candies are left , take a candies between 1 and {numsOfCandies}')
     if numsOfCandies < takenCandies:
         while takenCandies <= 0 or takenCandies > numsOfCandies:
-            takenCandies = int(input(f'{numsOfCandies} candies are left, take a candies between 1 and {numsOfCandies}: '))
+            takenCandies = int(
+                input(f'{numsOfCandies} candies are left, take a candies between 1 and {numsOfCandies}: '))
     numsOfCandies -= takenCandies
     return numsOfCandies
 
 
+# Добавление другой метода для выбора копмьютером значений.
 def computer_choice(numsOfCandies):
-    takenCandies = randint(1, 28)
-    while takenCandies > numsOfCandies:
-        takenCandies = randint(1, 28)
-    if numsOfCandies <= 28:
-        takenCandies = numsOfCandies
-    if 28 < numsOfCandies < 56:
-        takenCandies = numsOfCandies - 29
-    numsOfCandies -= takenCandies
-    print(f'Computer takes {takenCandies} candies. Candies left: {numsOfCandies}')
+    bot_take = numsOfCandies % (max_take + 1)
+    if bot_take % 2 == 0:
+        bot_take = randint(1, max_take)
+    numsOfCandies -= bot_take
+    print(f'Computer takes {bot_take} candies. Candies left: {numsOfCandies}')
     return numsOfCandies
 
 
