9cda9fd422516d9c96723d687a43e1a1e9c17269
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-09-05 21:48:52 +0300

    Delete unused code

--- Lesson_5/RLE_coder.py
+++ Lesson_5/RLE_coder.py
@@ -24,21 +24,3 @@ with open('origin_file.txt', 'r') as source:
         for i in source:
             output.write(rle_encoder(i))
             print('Your encoded text was saved in result_file.txt')
-
-#
-# def rle_decoder(text):
-#     result = ''
-#     count = ''
-#     node = False
-#     for i in text:
-#         if i == '"':
-#             node = not node
-#             continue
-#         if node or not i.isdigit():
-#             result += ''.join([i for _ in range((int(count) if len(count) > 0 else 1))])
-#             count = ''
-#             continue
-#
-#         count += i
-#
-#     return result
